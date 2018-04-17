da57d1b72f18c05325280e2ed199740e7c1bdf47
robinsonbd2 <robinsonbd2@gmail.com>
2018-04-17 16:48:44 -0400

    Move psql db code to its own controller

---
+++ controllers/db/index.js
@@ -0,0 +1,21 @@
+const express     = require('express');
+const router      = new express.Router();
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -6,12 +6,7 @@ const PORT = process.env.PORT || 5000
 const apiRouter = require('./controllers/api');
 const lorem = require('./controllers/lorem');
 const times = require('./controllers/times');
-
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
+const psqldb = require('./controllers/db');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -23,15 +18,5 @@ express()
   .use('/api', apiRouter)
   .use('/lorem', lorem)
   .use('/times', times)
-  .get('/db', async (req, res) => {
-      try {
-        const client = await pool.connect()
-        const result = await client.query('SELECT * FROM test_table');
-        res.render('pages/db', result);
-        client.release();
-      } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-      }
-    })
+  .use('/db', psqldb)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
