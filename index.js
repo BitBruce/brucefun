25b4228b9420cfd8d3000ec8bf9a88deb24213fb
robinsonbd2 <robinsonbd2@gmail.com>
2018-04-17 16:31:41 -0400

    Add postgresql plugin

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const apiRouter = require('./controllers/api');
 const lorem = require('./controllers/lorem');
 const times = require('./controllers/times');
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -17,4 +23,15 @@ express()
   .use('/api', apiRouter)
   .use('/lorem', lorem)
   .use('/times', times)
+  .get('/db', async (req, res) => {
+      try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM test_table');
+        res.render('pages/db', result);
+        client.release();
+      } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+      }
+    })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
