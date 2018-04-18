7404acfe2da71b6c47acfce9b67a69cdc2ffbc2f
robinsonbd2 <robinsonbd2@gmail.com>
2018-04-18 17:01:41 -0400

    Clean gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,6 @@ var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
-// var concatCss = require('gulp-concat-css');
-// var cleanCss = require('gulp-clean-css');
 
 gulp.task('default', ['sass'], function() {
 
@@ -16,16 +14,7 @@ gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(concat('all.scss'))
     .pipe(sass().on('error', sass.logError))
-    // .pipe(gulp.dest('./public/stylesheets'))
-    // .pipe(concatCss('./public/stylesheets/*.css'))
     .pipe(cssnano())
     .pipe(rename('main.min.css'))
     .pipe(gulp.dest('./public/stylesheets'));
 });
-
-// .pipe(cleanCss({compatibility: 'ie8'}))
-// .pipe(rename({ suffix: '.min' }))
-
-// gulp.task('sass:watch', function () {
-//   gulp.watch('./sass/**/*.scss', ['sass']);
-// });
