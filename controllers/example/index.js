80ab57b96b9a5d0206cc5be9f51d4456d58c2c2e
robinsonbd2 <robinsonbd2@gmail.com>
2018-04-25 01:16:51 -0400

    Add 404 page, clean up pages and routing

---
+++ controllers/example/index.js
@@ -0,0 +1,22 @@
+const express     = require('express');
+const router      = new express.Router();
+
+//TODO: error routing
+
+// Optional parameter! ?p=
+router.get('/', function(req, res) {
+  let p = req.query.p;
+  if (p) {
+    let page = '../partials/examples/'+p+'.ejs';
+    res.render('pages/example', {page:page});
+  } else {
+    res.render('pages/example');
+  }
+});
+
+// router.get('/:page', function(req, res) {
+//   page: req.params.page;
+//   res.render('pages/dynamic', {page:page});
+// });
+
+module.exports = router;
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const apiRouter = require('./controllers/api');
 const lorem = require('./controllers/lorem');
 const times = require('./controllers/times');
 const psqldb = require('./controllers/db');
-const bootstrap = require('./controllers/bootstrap');
+const example = require('./controllers/example');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -15,11 +15,11 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
-  .get('/splash', (req, res) => res.render('pages/splash'))
-  .use('/bootstrap', bootstrap)
+  .get('/splash', (req, res) => res.render('partials/examples/splash'))
+  .use('/example', example)
   .use('/lorem', lorem)
-  .get('/example/:id', (req, res) => res.send('id ' + req.params.id))
   .use('/api', apiRouter)//scuffed
   .use('/times', times)
   .use('/db', psqldb)
+  .get('*', (req, res) => res.render('pages/notfound'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
